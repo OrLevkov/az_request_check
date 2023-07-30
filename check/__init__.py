2d79ce49b73d5d908df2ef763021ef617bd103f1
niryariv <niryariv@gmail.com>
2023-07-30 12:22:45 +0300

    pass URL via query string

--- check/__init__.py
+++ check/__init__.py
@@ -1,27 +1,34 @@
 import requests
+import logging
 
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('az_request_check initiated')
+    test_url = "http://google.com"
+    # logging.info('az_request_check initiated')
 
-    GOVMAP_URL = "https://open.govmap.gov.il/geoserver/opendata/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=opendata:PARCEL_ALL&cql_filter=GUSH_NUM={}+AND+PARCEL={}&outputFormat=application/json&SRSNAME=EPSG:4326"
+    # GOVMAP_URL = "https://open.govmap.gov.il/geoserver/opendata/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=opendata:PARCEL_ALL&cql_filter=GUSH_NUM={}+AND+PARCEL={}&outputFormat=application/json&SRSNAME=EPSG:4326"
 
-    gush    = req.params.get('gush')
-    helka   = req.params.get('helka')
+    if len(req.params) > 0:
+        query_params = req.params
+        # Convert the dictionary to a string representation
+        test_url = "&".join([f"{key}={value}" for key, value in query_params.items()])
+    else:
+        test_url = "http://google.com"  
 
+    
 
-    test_url = GOVMAP_URL % (gush, helka)
+    # test_url = GOVMAP_URL % (gush, helka)
     logging.info(f'test_url: {test_url}')
 
     try:
         response = requests.get(test_url)
         logging.info(f'response: {response}')
-        output = response.json()
+        output = response.text
     except Exception as e:
         logging.error(f'Exception: {e}')
 
     return func.HttpResponse(
-        json.dumps(output), status_code=200
+        output, status_code=200
     )
